bef8f4f80c50b2886a5b375457022eaa409fca51
Ali Hamdi <ahamdi@exoplatform.com>
2018-09-25 17:49:32 +0100

    Added test services with configuration

--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,9 +1,27 @@
 package org.exoplatform.addons;
 
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
 /**
  * Created by eXo Platform SAS.
  *
+ * @author Ali Hamdi <ahamdi@exoplatform.com>
+ * @since 13/09/18 15:12
  */
 
 public class MyService {
+  /**
+   * The logger.
+   */
+  private static final Log LOG = ExoLogger.getExoLogger(MyStartableService.class);
+
+
+  public MyService() {
+     LOG.info("##################### Simple service started successfully !!");
+  }
+
+  public void call() {
+    LOG.info("######################  call function invoked !!");
+  }
 }
---
+++ services/src/main/java/org/exoplatform/addons/MyStartableService.java
@@ -0,0 +1,33 @@
+package org.exoplatform.addons;
+
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.container.PortalContainer;
+import org.picocontainer.Startable;
+import org.exoplatform.services.log.*;
+
+/**
+ * Created by eXo Platform SAS.
+ *
+ */
+
+public class MyStartableService implements Startable {
+  /**
+   * The logger.
+   */
+  private static final Log LOG = ExoLogger.getExoLogger(MyStartableService.class);
+
+  @Override
+  public void start() {
+    LOG.info("&&&&&&&&&&&& My Startable service started !");
+    /*PortalContainer portalContainer = (PortalContainer) ExoContainerContext.getCurrentContainer();
+    LOG.info("&&&&&&&&&&&& Getting an instance of MyService !");
+    MyService myService = (MyService) portalContainer.getComponentInstance(MyService.class);
+    LOG.info("&&&&&&&&&&&& Calling function call of MyService !");
+    myService.call();*/
+  }
+
+  @Override
+  public void stop() {
+    LOG.info("&&&&&&&&&&&& My Startable service stopped !");
+  }
+}
